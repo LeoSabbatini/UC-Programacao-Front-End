8c1a07339ef8ac563fbe205b95ae602ac8557617
Leonardo <leonardoherrerasabbatini@gmail.com>
2026-08-03 14:58:11 -0300

    ex7

---
+++ aluno/exercicios/condicionais/ex7/ex7.js
@@ -0,0 +1,18 @@
+let valor = Number(prompt("digite um valor"))
+
+
+if(valor%2 == 0){
+    if(valor >= 0){
+        alert("Valor par e positivo")
+    }
+    else{
+        alert("Valor par e negativo")
+    }
+}else{
+    if(valor >= 0){
+        alert("Valor impar e positivo")
+    }
+    else{
+        alert("Valor impar e negativo")
+    }
+}
